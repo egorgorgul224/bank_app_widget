668aeac337f6bfe266e2a8a5c7d17257982c489d
egor <scudf24@gmail.com>
2025-01-16 21:13:12 +0300

    fixes main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+
 from src.widget import mask_account_card, get_date
 
 card_number = input("Введите номер карты или счета: ")
 print(mask_account_card(card_number))
+
+print(get_date(str(datetime.today())))
