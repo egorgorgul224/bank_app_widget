bf44dab00c2b2501f7eb7095c51769151b33cb60
egor <scudf24@gmail.com>
2025-01-30 20:57:47 +0300

    add test_masks file and test functions, update functions in masks fyle

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,22 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция возвращает замаскированный номер карты в формате XXXX XX** **** XXXX"""
 
-    return f"{card_number[0:4]} {card_number[5:7]}** **** {card_number[-4:]}"
+    # if not isinstance(card_number, str):
+    #     raise TypeError("Ошибка типа данных")
+
+    if card_number.isdigit() and len(card_number) == 16:
+        return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    else:
+        return "Вы ввели неверный номер карты"
 
 
 def get_mask_account(account_number: str) -> str:
     """Функция возвращает замаскированный номер банковского счета в формате **XXXX, где X - последние 4 цифры счета"""
 
-    return "**" + account_number[-4:]
+    # if not isinstance(card_number, str):
+    #     raise TypeError("Ошибка типа данных")
+
+    if account_number.isdigit() and len(account_number) == 20:
+        return "**" + account_number[-4:]
+    else:
+        return "Вы ввели неверный номер счета"
