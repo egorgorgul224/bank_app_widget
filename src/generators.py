ab3a1d336c9892011591e5b74ef088dc9b04a366
egor <scudf24@gmail.com>
2025-02-07 18:24:51 +0300

    add transaction descriptions function, update main file

--- src/generators.py
+++ src/generators.py
@@ -1,4 +1,4 @@
-from typing import Generator
+from typing import Generator, Any
 
 
 def filter_by_currency(transactions_list: list[dict], currency: str) -> Generator[dict]:
@@ -10,3 +10,10 @@ def filter_by_currency(transactions_list: list[dict], currency: str) -> Generato
     for transaction in transactions_list:
         if transaction.get("operationAmount", {}).get("currency", {}).get("name") == currency:
             yield transaction
+
+
+def transaction_descriptions(transactions_list: list[dict]) -> Generator[Any]:
+    """Функция принимает список транзакций и возвращает описание операций по очереди"""
+
+    for transaction in transactions_list:
+        yield transaction.get("description")
