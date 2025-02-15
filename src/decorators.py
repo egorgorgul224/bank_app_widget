5aa8af570841b76e200a83b4a7efcc33afcd0dea
egor <scudf24@gmail.com>
2025-02-15 12:18:05 +0300

    add decorators tests, update decorator log

--- src/decorators.py
+++ src/decorators.py
@@ -1,48 +1,41 @@
 from typing import Any, Callable
 
 
-def int_sum(func):
-    """Декоратор возвращает лог с названием функции, результатом, если функция отрабатывает корректно.
-    Если функция отрабатывает некорректно, то лог возвращает название функции, ошибку и входные данные."""
+def log(filename: str = ""):
+    """Декоратор возвращает лог с названием функции и результатом, если функция отрабатывает корректно.
+    Если функция отрабатывает некорректно, то лог возвращает название функции, ошибку и входные данные.
+    Лог отправляется в текстовый файл, если файл передан в параметр, иначе выводит лог в консоль."""
 
-    def wrapper(*args):
-        try:
-            result = func(*args)
-            return f"{func.__name__} ok. Result: {result}\n"
-        except ZeroDivisionError as zero_log:
-            return f"{func.__name__} error: {zero_log}. Inputs: {args}.\n"
-        except FileNotFoundError as file_log:
-            return f"{func.__name__} error: {file_log}. Inputs: {args}.\n"
-        except TypeError as type_log:
-            return f"{func.__name__} error: {type_log}. Inputs: {args}.\n"
-        except Exception as exc_log:
-            return f"{func.__name__} error: {exc_log}. Inputs: {args}.\n"
+    def sum_args(func):
 
-    return wrapper
-
-
-def log(filename=None):
-    """Декоратор принимает лог с результатом функции и записывает его в лог-файл, если лог-файл указан в параметре.
-    Если лог-файл не указан, то возвращает лог в консоль."""
-
-    def decorator(func):
         def wrapper(*args):
-            total_message = func(*args)
+            try:
+                result = func(*args)
+                message = f"{func.__name__} ok. Result: {result}."
+            except ZeroDivisionError as zero_log:
+                message = f"{func.__name__} error: {zero_log}. Inputs: {args}."
+            except FileNotFoundError as file_log:
+                message = f"{func.__name__} error: {file_log}. Inputs: {args}."
+            except TypeError as type_log:
+                message = f"{func.__name__} error: {type_log}. Inputs: {args}."
+            except Exception as exc_log:
+                message = f"{func.__name__} error: {exc_log}. Inputs: {args}."
+
             if filename:
-                with open("mylog.txt", "a", encoding="utf-8") as file:
-                    file.write(f"{total_message}")
+                with open(filename, "a", encoding="utf-8") as file:
+                    file.write(f"{message}\n")
             else:
-                print(f"{total_message}")
+                print(message)
+                return message
 
         return wrapper
 
-    return decorator
+    return sum_args
 
 
-@log(filename="mylog.txt")
-@int_sum
+@log()
 def my_function(x, y):
-    return x + y
+    return x / y
 
 
 my_function(1, 2)
---
+++ tests/test_decorators.py
@@ -0,0 +1,88 @@
+import os
+import tempfile
+
+import pytest
+
+from src.decorators import log, my_function
+
+
+def test_my_function_logs(capsys):
+    """Тест проверяет корректную работу функции my_function"""
+    my_function(1, 2)
+    captured = capsys.readouterr()
+    assert captured.out == "my_function ok. Result: 0.5.\n"
+
+
+def test_my_function_type_error(capsys):
+    """Тест проверяет, что при передаче разных типов данных выводится ошибка unsupported operand type(s)"""
+    my_function(1, "2")
+    captured = capsys.readouterr()
+    assert captured.out == "my_function error: unsupported operand type(s) for /: 'int' and 'str'. Inputs: (1, '2').\n"
+
+
+def test_my_function_zero_error(capsys):
+    """Тест проверяет, что при делении на 0 выводится ошибка division by zero"""
+    my_function(1, 0)
+    captured = capsys.readouterr()
+    # assert "my_function error: division by zero. Inputs: (1, 0)." in captured.out
+    assert captured.out == "my_function error: division by zero. Inputs: (1, 0).\n"
+
+
+def test_log_correct():
+    """Тест проверяет корректную работу декоратор log"""
+
+    @log()
+    def my_function(x, y):
+        return x / y
+
+    result = my_function(1, 2)
+    assert result == "my_function ok. Result: 0.5."
+
+
+def test_log_error():
+    """Тест проверяет, что декоратор log выводит ошибку division by zero при делении на 0"""
+
+    @log()
+    def my_function(x, y):
+        return x / y
+
+    result = my_function(1, 0)
+    assert result == "my_function error: division by zero. Inputs: (1, 0)."
+
+
+def test_log_correct_to_file():
+    """Тест проверяет, что декоратор log пердает корректный результат в файл mylog.txt"""
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        temp_filename = temp_file.name
+    try:
+
+        @log(filename=temp_filename)
+        def my_function(x, y):
+            return x / y
+
+        my_function(1, 2)
+
+        with open(temp_filename, "r") as file:
+            log_content = file.read()
+        assert "my_function ok. Result: 0.5.\n" in log_content
+    finally:
+        os.remove(temp_filename)
+
+
+def test_log_error_to_file():
+    """Тест проверяет, что декоратор log пердает ошибку division by zero в файл mylog.txt"""
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        temp_filename = temp_file.name
+    try:
+
+        @log(filename=temp_filename)
+        def my_function(x, y):
+            return x / y
+
+        my_function(1, 0)
+
+        with open(temp_filename, "r") as file:
+            log_content = file.read()
+        assert "my_function error: division by zero. Inputs: (1, 0).\n" in log_content
+    finally:
+        os.remove(temp_filename)
